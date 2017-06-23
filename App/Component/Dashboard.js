e1e69d899dfde3706b06d94bbe8de52d5f060a86
Raunaqss <raunaqss@gmail.com>
2017-06-24 02:02:02 +0530

    Complete Dashboard View with Image and TouchableHighlights with reusable styles

--- App/Component/Dashboard.js
+++ App/Component/Dashboard.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
-  StyleSheet
+  StyleSheet,
+  Image,
+  TouchableHighlight
 } from 'react-native';
 
 var styles = StyleSheet.create({
@@ -21,11 +23,58 @@ var styles = StyleSheet.create({
 });
 
 export default class Dashboard extends Component {
+  makeBackground(btn) {
+    var obj = {
+      flexDirection: 'row',
+      alignSelf: 'stretch',
+      justifyContent: 'center',
+      flex: 1
+    };
+
+    if (btn === 0) {
+      obj.backgroundColor = '#48BBEC';
+    } else if (btn === 1) {
+      obj.backgroundColor = '#E77AAE';
+    } else {
+      obj.backgroundColor = '#758BF4';
+    }
+
+    return obj;
+  }
+  goToProfile() {
+    console.log('Go to profile');
+  }
+  goToRepos() {
+    console.log('Go to repos');
+  }
+  goToNotes() {
+    console.log('Go to notes');
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text> This is a dashboard. Yeah. </Text>
-        <Text> {JSON.stringify(this.props.userInfo)} </Text>
+        <Image
+          source={{uri: this.props.userInfo.avatar_url}}
+          style={styles.image}
+        />
+        <TouchableHighlight
+          style={this.makeBackground(0)}
+          onPress={this.goToProfile.bind(this)}
+        >
+          <Text style={styles.buttonText}>View Profile</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={this.makeBackground(1)}
+          onPress={this.goToRepos.bind(this)}
+        >
+          <Text style={styles.buttonText}>View Repos</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={this.makeBackground(2)}
+          onPress={this.goToNotes.bind(this)}
+        >
+          <Text style={styles.buttonText}>View Notes</Text>
+        </TouchableHighlight>
       </View>
     )
   }
