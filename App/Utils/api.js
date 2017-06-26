ca304cf6ac0f54be9990f1f6b1ed9e30f27dafa1
Raunaqss <raunaqss@gmail.com>
2017-06-26 16:42:58 +0530

    Add methods to api to interact with notes on firebaseio database using restful api

--- App/Utils/api.js
+++ App/Utils/api.js
@@ -8,5 +8,18 @@ export var api = {
     username = username.toLowerCase().trim();
     var url = `https://api.github.com/users/${username}/repos`;
     return fetch(url).then((res) => res.json());
+  },
+  getNotes(username) {
+    username = username.toLowerCase().trim();
+    var url = `https://github-notetaker-d5642.firebaseio.com/${username}.json`;
+    return fetch(url).then((res) => res.json());
+  },
+  addNote(username, note) {
+    username = username.toLowerCase().trim();
+    var url = `https://github-notetaker-d5642.firebaseio.com/${username}.json`;
+    return fetch(url, {
+      method: 'post',
+      body: JSON.stringify(note)
+    }).then((res) => res.json());
   }
 };
