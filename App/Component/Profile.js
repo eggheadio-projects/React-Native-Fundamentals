9aae34a84bbdcf4ba70423a91f5e6af88b66b36c
Raunaqss <raunaqss@gmail.com>
2017-06-26 12:52:59 +0530

    Remove unnecessary argument to helper function

--- App/Component/Profile.js
+++ App/Component/Profile.js
@@ -30,7 +30,7 @@ var styles = StyleSheet.create({
 });
 
 export default class Profile extends Component {
-  getRowTitle(user, item) {
+  getRowTitle(item) {
     var item = (item === 'public_repos') ? item.replace('_', ' ') : item;
     return item[0] ? item[0].toUpperCase() + item.slice(1) : item;
   }
@@ -44,7 +44,7 @@ export default class Profile extends Component {
       } else {
         return (
           <View key={index} style={styles.rowContainer}>
-            <Text style={styles.rowTitle}> {this.getRowTitle(userInfo, item)} </Text>
+            <Text style={styles.rowTitle}> {this.getRowTitle(item)} </Text>
             <Text style={styles.rowContent}> {userInfo[item]} </Text>
           </View>
         )
