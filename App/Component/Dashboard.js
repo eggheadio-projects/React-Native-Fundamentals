04a91716fe6cf021283e9122cb80a9a08a0fcb2f
Raunaqss <raunaqss@gmail.com>
2017-06-27 11:40:51 +0530

    Add Notes screen communicating with firebase database

--- App/Component/Dashboard.js
+++ App/Component/Dashboard.js
@@ -8,6 +8,7 @@ import {
 } from 'react-native';
 import Profile from './Profile';
 import Repositories from './Repositories';
+import Notes from './Notes';
 import { api } from '../Utils/api';
 
 var styles = StyleSheet.create({
@@ -62,7 +63,15 @@ export default class Dashboard extends Component {
       });
   }
   goToNotes() {
-    console.log('Go to notes');
+    api.getNotes(this.props.userInfo.login)
+      .then((res) => {
+        res = res || {};
+        this.props.navigator.push({
+          title: 'Notes',
+          component: Notes,
+          passProps: { userInfo: this.props.userInfo, notes: res }
+        });
+      });
   }
   render() {
     return (
